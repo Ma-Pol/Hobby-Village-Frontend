a623eb476b45fa1b47bb5dce04c2596825f9b3ed
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-04-27 10:29:17 +0900

    프로젝트 초기 설정 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      {/* <Route path="/test1" element={<Test1 />} /> */}
+      {/* <Route path="/test2" element={<Test2 />} /> */}
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
