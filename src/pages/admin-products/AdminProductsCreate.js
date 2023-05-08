62e526e70884145417ecd316850900a40f502607
hnbae <hnbae96@gmail.com>
2023-05-09 01:21:31 +0900

    관리자 상품등록페이지 ui 완료

--- src/pages/admin-products/AdminProductsCreate.js
+++ src/pages/admin-products/AdminProductsCreate.js
@@ -1,10 +1,343 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import SimpleImageSlider from 'react-simple-image-slider';
+import {
+  Container,
+  Box,
+  Typography,
+  Table,
+  TableContainer,
+  TableCell,
+  TableRow,
+  InputLabel,
+  TextField,
+  MenuItem,
+  Grid,
+  Button,
+  Input,
+  InputAdornment,
+} from '@mui/material';
 
 const AdminProductsCreate = () => {
+  const prodCodeRef = useRef();
+  const prodBrandRef = useRef();
+  const prodPriceRef = useRef();
+  const prodCategoryRef = useRef();
+  const prodShippingRef = useRef(); // 배송비
+  const prodNameRef = useRef();
+  const prodPictureRef = useRef();
+  const prodContentRef = useRef();
+  const prodHostRef = useRef();
+  const prodTagRef = useRef();
+
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  const tableHeadStyle = {
+    width: 170,
+    fontSize: 18,
+    border: '1px solid #E0E0E0',
+  };
+
+  const tableBodyStyle = { width: 400, border: '1px solid #E0E0E0' };
+
+  const inputStyle = {
+    '& .MuiOutlinedInput-root.Mui-focused': {
+      '& > fieldset': {
+        borderColor: '#c3c36a',
+      },
+    },
+  };
+
+  const btnUploadImageStyle = {
+    display: 'block',
+    width: '60px',
+    height: '20px',
+    bgcolor: '#ECECEC',
+    fontSize: '12px',
+    color: '#4E4E4E',
+    border: 'none',
+    borderRadius: '5px',
+    textAlign: 'center',
+    lineHeight: '190%',
+    boxShadow: '2px 2px 2px 1px #b0b0b0',
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  };
+
+  const btnSubmitStyle = {
+    width: '65px',
+    height: '30px',
+    bgcolor: '#c3c36a',
+    borderRadius: '15px',
+    border: '1px solid #626262',
+    color: '#000000',
+    fontWeight: 'bold',
+    '&:hover': {
+      bgcolor: '#c3c36a',
+      border: '1px solid #626262',
+      color: '#ffffff',
+    },
+  };
+
+  const btnCancelStyle = {
+    width: '65px',
+    height: '30px',
+    bgcolor: '#ffffff',
+    borderRadius: '15px',
+    border: '1px solid #626262',
+    color: '#000000',
+    fontWeight: 'bold',
+    '&:hover': {
+      bgcolor: '#ffffff',
+      border: '1px solid #626262',
+      color: '#000000',
+    },
+  };
+
+  const images = [
+    {
+      url: "process.env.PUBLIC_URL + '/arena.png'",
+    },
+    {
+      url: "process.env.PUBLIC_URL + '/brandyarn.png'",
+    },
+    {
+      url: "process.env.PUBLIC_URL + '/excider.png'",
+    },
+  ];
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <Container>
+      {/* 타이틀 */}
+      <Typography
+        variant="h4"
+        component="h1"
+        sx={{
+          mt: 5,
+          mb: 5,
+          pl: 1,
+          pr: 1,
+          fontWeight: 'bold',
+        }}
+      >
+        상품 목록 &#62; 상품 등록
+      </Typography>
+
+      {/* form 시작 */}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <TableContainer>
+          <Table sx={{ maxWidth: 1140 }}>
+            <TableRow>
+              <TableCell sx={tableHeadStyle}>
+                <InputLabel for="prodCode">상품 코드</InputLabel>
+              </TableCell>
+              <TableCell sx={tableBodyStyle}>
+                <TextField
+                  id="prodCode"
+                  fullWidth
+                  size="small"
+                  ref={prodCodeRef}
+                  sx={inputStyle}
+                ></TextField>
+              </TableCell>
+              <TableCell sx={tableHeadStyle}>
+                <InputLabel for="prodBrand">브랜드</InputLabel>
+              </TableCell>
+              <TableCell sx={tableBodyStyle}>
+                <TextField
+                  id="prodBrand"
+                  select
+                  defaultValue={1}
+                  fullWidth
+                  size="small"
+                  ref={prodBrandRef}
+                  sx={inputStyle}
+                >
+                  <MenuItem key={1} value={1}>
+                    브랜드 선택
+                  </MenuItem>
+                  <MenuItem key="아레나" value="아레나">
+                    아레나
+                  </MenuItem>
+                </TextField>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={tableHeadStyle}>
+                <InputLabel for="prodPrice">대여 가격</InputLabel>
+              </TableCell>
+              <TableCell sx={tableBodyStyle}>
+                <TextField
+                  id="prodPrice"
+                  fullWidth
+                  size="small"
+                  ref={prodPriceRef}
+                  sx={inputStyle}
+                  label="숫자만 입력"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">&#8361;</InputAdornment>
+                    ),
+                  }}
+                ></TextField>
+              </TableCell>
+              <TableCell sx={tableHeadStyle}>
+                <InputLabel for="prodCategory">카테고리</InputLabel>
+              </TableCell>
+              <TableCell sx={tableBodyStyle}>
+                <TextField
+                  id="prodCategory"
+                  select
+                  defaultValue={0}
+                  fullWidth
+                  size="small"
+                  ref={prodCategoryRef}
+                  sx={inputStyle}
+                >
+                  <MenuItem key={0} value={0}>
+                    카테고리 선택
+                  </MenuItem>
+                  <MenuItem key="등산" value="등산">
+                    등산
+                  </MenuItem>
+                </TextField>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={tableHeadStyle}>
+                <InputLabel for="prodShipping">배송비</InputLabel>
+              </TableCell>
+              <TableCell sx={tableBodyStyle}>
+                <TextField
+                  id="prodShipping"
+                  fullWidth
+                  size="small"
+                  ref={prodShippingRef}
+                  sx={inputStyle}
+                  label="숫자만 입력"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">&#8361;</InputAdornment>
+                    ),
+                  }}
+                ></TextField>
+              </TableCell>
+              <TableCell sx={tableHeadStyle}>
+                <InputLabel for="prodName">상품명</InputLabel>
+              </TableCell>
+              <TableCell sx={tableBodyStyle}>
+                <TextField
+                  id="prodName"
+                  fullWidth
+                  size="small"
+                  ref={prodNameRef}
+                  sx={inputStyle}
+                ></TextField>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={tableHeadStyle}>
+                <InputLabel>상품 사진</InputLabel>
+                {/* 파일선택 버튼: label로 연결, 실제 input은 숨김 */}
+                <InputLabel
+                  id="btnUploadImage"
+                  for="prodPicture"
+                  sx={btnUploadImageStyle}
+                >
+                  파일선택
+                </InputLabel>
+                <Input
+                  id="prodPicture"
+                  ref={prodPictureRef}
+                  type="file"
+                  accept="image/*"
+                  inputProps={{ multiple: true }}
+                  sx={{ display: 'none' }}
+                ></Input>
+              </TableCell>
+              <TableCell sx={tableBodyStyle}>
+                <SimpleImageSlider
+                  width={220}
+                  height={220}
+                  images={images}
+                  showBullets={true}
+                  showNavs={true}
+                  navSize={15}
+                  navMargin={10}
+                />
+              </TableCell>
+              <TableCell sx={tableHeadStyle}>
+                <InputLabel for="prodContent">상품 설명</InputLabel>
+              </TableCell>
+              <TableCell sx={tableBodyStyle}>
+                <TextField
+                  id="prodContent"
+                  fullWidth
+                  size="small"
+                  ref={prodCategoryRef}
+                  sx={inputStyle}
+                  multiline
+                  inputProps={{ style: { height: '200px' } }}
+                ></TextField>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={tableHeadStyle}>
+                <InputLabel for="prodHost">대여자 닉네임</InputLabel>
+              </TableCell>
+              <TableCell sx={tableBodyStyle}>
+                <TextField
+                  id="prodHost"
+                  fullWidth
+                  size="small"
+                  ref={prodHostRef}
+                  sx={inputStyle}
+                ></TextField>
+              </TableCell>
+              <TableCell sx={tableHeadStyle}>
+                <InputLabel for="prodTag">연관검색어</InputLabel>
+              </TableCell>
+              <TableCell sx={tableBodyStyle}>
+                <TextField
+                  id="prodTag"
+                  fullWidth
+                  size="small"
+                  ref={prodContentRef}
+                  sx={inputStyle}
+                  multiline
+                ></TextField>
+              </TableCell>
+            </TableRow>
+          </Table>
+        </TableContainer>
+
+        {/* 하단 버튼 */}
+        <Box
+          sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
+          mt={5}
+        >
+          <Button type="submit" variant="outlined" mr={3} sx={btnSubmitStyle}>
+            등록
+          </Button>
+          &nbsp;&nbsp;&nbsp;
+          <Button variant="outlined" onClick={handleGoBack} sx={btnCancelStyle}>
+            취소
+          </Button>
+        </Box>
+      </Box>
+      {/* form 끝 */}
+    </Container>
   );
 };
 
--- src/pages/user-products/UserProductsList.js
+++ src/pages/user-products/UserProductsList.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Box, TextField } from '@mui/material';
 
 const UserProductsList = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <Box>
+      <Box name="category">
+        <TextField>전체</TextField>
+        <TextField>등산 수영 웨이트 캠핑 테니스 뜨개 사진 악기 기타</TextField>
+      </Box>
+      <Box name="filter"></Box>
+      <Box name="produccts"></Box>
+    </Box>
   );
 };
 
