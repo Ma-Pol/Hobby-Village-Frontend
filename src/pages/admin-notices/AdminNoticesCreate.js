174681a7122a6757bdbf5a3f2ca0625e52285d03
paul3148 <paul3148@gmail.com>
2023-05-12 14:37:11 +0900

    공지사항 등록 기능 구현

--- src/pages/admin-notices/AdminNoticesCreate.js
+++ src/pages/admin-notices/AdminNoticesCreate.js
@@ -1,10 +1,216 @@
-import React from 'react';
+import {
+  Box,
+  Container,
+  Typography,
+  TextField,
+  FormControl,
+  InputLabel,
+  handleChange,
+  Button,
+  Select,
+  MenuItem,
+} from '@mui/material';
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const AdminNoticesCreate = () => {
+  const [notCategory, setNotCategory] = useState();
+
+  const handleChange = (event) => {
+    setNotCategory(event.target.value);
+  };
+
+  const noticeTitleRef = useRef();
+  const noticeContentRef = useRef();
+
+  // const token = document.getElementById('token');
+
+  const navigate = useNavigate();
+
+  const insertNotice = () => {
+    axios
+      .post(`/m/notices/noticeCreate`, {
+        notTitle: noticeTitleRef.current.value,
+        notCategory: notCategory,
+        notContent: noticeContentRef.current.value,
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.data === 1) {
+          navigate('/m/notices/lists?sort=-notDate&filter=none&pages=1');
+        } else {
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {});
+
+  const noticeRow = {
+    minWidth: '120px',
+    display: 'flex',
+  };
+
+  const noticeFirstCell = {
+    width: '100px',
+    display: 'flex',
+    alignItems: 'center',
+    // 세로 가운데정렬
+    // justifyContents: 'center' - 가로 가운데정렬
+  };
+
+  const noticeContentCell = {
+    width: '100px',
+    display: 'flex',
+  };
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <Container>
+      {/* 공지사항 등록 글씨 표기 시작 */}
+      <Typography
+        variant="h4"
+        component="h1"
+        sx={{
+          mt: 5,
+          mb: 1,
+          pl: 2,
+          pr: 1,
+          fontWeight: 'bold',
+          userSelect: 'none',
+        }}
+      >
+        공지사항 &#62; 등록
+      </Typography>
+
+      <Box sx={noticeRow}>
+        <Box sx={noticeFirstCell}>
+          <Typography variant="h6" component="h2" sx={{ fontWeight: 'bold' }}>
+            제목
+          </Typography>
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              id="outlined-size-small"
+              size="small"
+              inputRef={noticeTitleRef}
+            />
+          </div>
+        </Box>
+      </Box>
+
+      <Box sx={noticeRow}>
+        <Box sx={noticeFirstCell}>
+          <Typography variant="h6" component="h2" sx={{ fontWeight: 'bold' }}>
+            카테고리
+          </Typography>
+        </Box>
+        <FormControl
+          sx={{
+            width: '15ch',
+            mx: 1,
+          }}
+        >
+          <InputLabel id="demo-simple-select-label"></InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            size="small"
+            value={notCategory}
+            label="전체"
+            onChange={handleChange}
+          >
+            <MenuItem value="전체">전체</MenuItem>
+            <MenuItem value="안내">안내</MenuItem>
+            <MenuItem value="이벤트">이벤트</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+
+      <Box sx={noticeRow}>
+        <Box sx={noticeContentCell}>
+          <Typography variant="h6" component="h2" sx={{ fontWeight: 'bold' }}>
+            내용
+          </Typography>
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '100ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              id="outlined-size-small"
+              size="small"
+              multiline
+              rows={6}
+              inputRef={noticeContentRef}
+            />
+          </div>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'Center',
+        }}
+      >
+        <Button
+          variant="contained"
+          size="small"
+          href="/m/notices/create"
+          sx={{
+            mt: 1,
+            mr: 1,
+            height: '25px',
+            float: 'center',
+            backgroundColor: '#FFFFFF',
+            color: '#000000',
+            border: '1px solid #000000',
+            '&:hover': {
+              backgroundColor: '#c6c6c6',
+              color: '#000000',
+            },
+          }}
+        >
+          취소
+        </Button>
+        <Button
+          variant="contained"
+          size="small"
+          onClick={insertNotice}
+          sx={{
+            mt: 1,
+            mr: 1,
+            height: '25px',
+            float: 'center',
+            backgroundColor: '#C3C36A',
+            color: '#000000',
+            border: '1px solid #000000',
+            '&:hover': {
+              backgroundColor: '#c6c6c6',
+              color: '#000000',
+            },
+          }}
+        >
+          등록
+        </Button>
+      </Box>
+    </Container>
   );
 };
 
--- src/pages/admin-notices/AdminNoticesDetails.js
+++ src/pages/admin-notices/AdminNoticesDetails.js
@@ -1,17 +1,5 @@
-import {
-  Box,
-  Container,
-  Grid,
-  NativeSelect,
-  Typography,
-  Button,
-  InputLabel,
-  Pagination,
-  TextField,
-  ToggleButtonGroup,
-  ToggleButton,
-} from '@mui/material';
-import { findByLabelText } from '@testing-library/react';
+import { Box, Container, Typography, Button } from '@mui/material';
+
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
@@ -38,11 +26,11 @@ const AdminNoticesDetails = () => {
 
   const noticeDetailFirstCell = {
     width: '100px',
-  }
+  };
 
   const noticeDetailSecondCell = {
     width: 'calc(100% - 100px)',
-  }
+  };
 
   return (
     <Container>
@@ -62,135 +50,169 @@ const AdminNoticesDetails = () => {
         공지사항 &#62; 상세
       </Typography>
 
-    <Box
-      sx={{
-        mt: 2,
-        mx: 'auto',
-        px: 2,
-        width: '1100px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-
-    <Box sx={{
-      backgroundColor: '#DDDDDD',
-      width: '1100px',
-    }} >
-
-    <Box sx={noticeDetailRow}>
-      <Box sx = {noticeDetailFirstCell}>
-        <Typography variant="h6" component="h2" sx={{fontWeight: 'bold'}}>제목</Typography>
-      </Box>
-      <Box sx = {noticeDetailSecondCell}>
-        <Typography variant="h6" component="h2">{noticeDetail.notTitle}</Typography>
-       </Box>
-    </Box>
-
-    <Box sx={noticeDetailRow}>
-      <Box sx = {noticeDetailFirstCell}>
-         <Typography variant="h6" component="h2" sx={{fontWeight: 'bold'}}>카테고리</Typography>
-      </Box>
-
-      <Box sx = {noticeDetailSecondCell}>
-        <Typography variant="h6" component="h2">{noticeDetail.notCategory}</Typography>
-       </Box>
-    </Box>
-
-    <Box sx={noticeDetailRow}>
-      <Box sx = {noticeDetailFirstCell}>
-        <Typography variant="h6" component="h2" sx={{fontWeight: 'bold'}}>작성일</Typography>
-      </Box>
-
-      <Box sx = {noticeDetailSecondCell}>
-        <Typography variant="h6" component="h2">{noticeDetail.notDate}</Typography>
-      </Box>
-    </Box>
-
-    <Box sx={noticeDetailRow}>
-      <Box sx = {noticeDetailFirstCell}>
-      <Typography variant="h6" component="h2" sx={{fontWeight: 'bold'}}>내용</Typography>
-      </Box>
-
-      <Box sx = {noticeDetailSecondCell}>
-        <Typography dangerouslySetInnerHTML={{ __html: noticeDetail.notContent }} variant="h6" component="h2"></Typography>
-      </Box>
-    </Box>
-  </Box>
-</Box>
-
-<Box sx={{
-  display: 'flex',
-  justifyContent: 'Center',
-}}>
-  
-      <Button
-        variant="contained"
-        size="small"
-        href="/m/notices/delete"  
+      <Box
         sx={{
-          mt: 1,
-          mr: 1,
-          height: '25px',
-          float: 'center',
-          backgroundColor: '#F5B8B8',
-          color: '#000000',
-          border: '1px solid #000000',
-          '&:hover': {
-            backgroundColor: '#c6c6c6',
-            color: '#000000',
-          },
+          mt: 2,
+          mx: 'auto',
+          px: 2,
+          width: '1100px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        삭제
-      </Button>
+        <Box
+          sx={{
+            backgroundColor: '#DDDDDD',
+            width: '1100px',
+          }}
+        >
+          <Box sx={noticeDetailRow}>
+            <Box sx={noticeDetailFirstCell}>
+              <Typography
+                variant="h6"
+                component="h2"
+                sx={{ fontWeight: 'bold' }}
+              >
+                제목
+              </Typography>
+            </Box>
+            <Box sx={noticeDetailSecondCell}>
+              <Typography variant="h6" component="h2">
+                {noticeDetail.notTitle}
+              </Typography>
+            </Box>
+          </Box>
 
-      <Button
-        variant="contained"
-        size="small"
-        href="/m/notices/lists"
+          <Box sx={noticeDetailRow}>
+            <Box sx={noticeDetailFirstCell}>
+              <Typography
+                variant="h6"
+                component="h2"
+                sx={{ fontWeight: 'bold' }}
+              >
+                카테고리
+              </Typography>
+            </Box>
+
+            <Box sx={noticeDetailSecondCell}>
+              <Typography variant="h6" component="h2">
+                {noticeDetail.notCategory}
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box sx={noticeDetailRow}>
+            <Box sx={noticeDetailFirstCell}>
+              <Typography
+                variant="h6"
+                component="h2"
+                sx={{ fontWeight: 'bold' }}
+              >
+                작성일
+              </Typography>
+            </Box>
+
+            <Box sx={noticeDetailSecondCell}>
+              <Typography variant="h6" component="h2">
+                {noticeDetail.notDate}
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box sx={noticeDetailRow}>
+            <Box sx={noticeDetailFirstCell}>
+              <Typography
+                variant="h6"
+                component="h2"
+                sx={{ fontWeight: 'bold' }}
+              >
+                내용
+              </Typography>
+            </Box>
+
+            <Box sx={noticeDetailSecondCell}>
+              <Typography
+                dangerouslySetInnerHTML={{ __html: noticeDetail.notContent }}
+                variant="h6"
+                component="h2"
+              ></Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box
         sx={{
-          mt: 1,
-          mr: 1,
-          height: '25px',
-          float: 'center',
-          backgroundColor: '#FFFFFF',
-          color: '#000000',
-          border: '1px solid #000000',
-          '&:hover': {
-            backgroundColor: '#c6c6c6',
-            color: '#000000',
-          },
+          display: 'flex',
+          justifyContent: 'Center',
         }}
       >
-        목록
-      </Button>
-
-      <Button
-        variant="contained"
-        size="small"
-        href="/m/notices/modify"
-        sx={{
-          mt: 1,
-          mr: 1,
-          height: '25px',
-          float: 'center',
-          backgroundColor: '#C3C36A',
-          color: '#000000',
-          border: '1px solid #000000',
-          '&:hover': {
-            backgroundColor: '#c6c6c6',
+        <Button
+          variant="contained"
+          size="small"
+          href="/m/notices/delete"
+          sx={{
+            mt: 1,
+            mr: 1,
+            height: '25px',
+            float: 'center',
+            backgroundColor: '#F5B8B8',
             color: '#000000',
-          },
-        }}
-      >
-        수정
-      </Button>
+            border: '1px solid #000000',
+            '&:hover': {
+              backgroundColor: '#c6c6c6',
+              color: '#000000',
+            },
+          }}
+        >
+          삭제
+        </Button>
+
+        <Button
+          variant="contained"
+          size="small"
+          href="/m/notices/lists"
+          sx={{
+            mt: 1,
+            mr: 1,
+            height: '25px',
+            float: 'center',
+            backgroundColor: '#FFFFFF',
+            color: '#000000',
+            border: '1px solid #000000',
+            '&:hover': {
+              backgroundColor: '#c6c6c6',
+              color: '#000000',
+            },
+          }}
+        >
+          목록
+        </Button>
+
+        <Button
+          variant="contained"
+          size="small"
+          href="/m/notices/modify"
+          sx={{
+            mt: 1,
+            mr: 1,
+            height: '25px',
+            float: 'center',
+            backgroundColor: '#C3C36A',
+            color: '#000000',
+            border: '1px solid #000000',
+            '&:hover': {
+              backgroundColor: '#c6c6c6',
+              color: '#000000',
+            },
+          }}
+        >
+          수정
+        </Button>
       </Box>
     </Container>
   );
 };
-
 
 export default AdminNoticesDetails;
